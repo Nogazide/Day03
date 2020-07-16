9c7a96013b2a7165fcc55be2f37acacf37a6316c
Nogazide <67911738+Nogazide@users.noreply.github.com>
2020-07-16 13:04:01 +0200

    Update ft_ft.c

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -11,22 +11,9 @@
 /* ************************************************************************** */
 
 
-#include <stdio.h>
-
 
 void ft_ft(int *nbr)
 {
 	*nbr= 42;
 }
 
-int main(void)
-{
-	int n;
-	
-	n = 5;
-	ft_ft(&n);
-
-	printf("%d", n);
-
-	return(0);
-}
